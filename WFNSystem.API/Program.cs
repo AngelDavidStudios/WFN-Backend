b97803d1f4ddd45cabd76fd663cf464971419d9f
Angel David <angeldavidstudios@outlook.com>
2025-11-09 02:03:31 -0500

    Configuracion adicional para ejecucion Lambda

--- WFNSystem.API/Program.cs
+++ WFNSystem.API/Program.cs
@@ -1,9 +1,24 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddCors();
+
+// Add AWS Lambda hosting
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+//Services AWS
+var awsOptions = builder.Configuration.GetAWSOptions();
+awsOptions.Profile = "AdminAccess";
+builder.Services.AddDefaultAWSOptions(awsOptions);
+builder.Services.AddAWSService<IAmazonDynamoDB>();
+builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
 
 var app = builder.Build();
 
